e5dbb04b977f6bea6632a1d31740d827a6cac34c
YangXian <1297624418@qq.com>
2020-07-11 17:20:55 +0800

    1.init工程

---
+++ app/src/main/java/swu/yx/a1app/MainActivity.java
@@ -0,0 +1,13 @@
+package swu.yx.a1app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
